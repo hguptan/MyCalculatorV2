fa363bb9314686add668aec28334d946e7721389
hnagariya <neelam.nagariya@gmail.com>
2024-12-16 10:21:40 -0500

    history_in_2ndActivity

--- app/src/main/java/com/example/mycalculatorv2/MainActivity.java
+++ app/src/main/java/com/example/mycalculatorv2/MainActivity.java
@@ -5,14 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     String result;
