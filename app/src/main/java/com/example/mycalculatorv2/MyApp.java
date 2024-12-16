a9858653f35d30628d99c1e10079d57c0a71c11b
hnagariya <neelam.nagariya@gmail.com>
2024-12-16 10:21:54 -0500

    history_in_2ndActivity

---
+++ app/src/main/java/com/example/mycalculatorv2/MyApp.java
@@ -0,0 +1,9 @@
+package com.example.mycalculatorv2;
+
+import android.app.Application;
+
+import java.util.ArrayList;
+
+public class MyApp extends Application {
+   static  ArrayList<String> history = new ArrayList<>(0);
+}
